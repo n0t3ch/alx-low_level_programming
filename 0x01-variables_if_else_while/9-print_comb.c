f109053f03a3f391fa31a8bed1ca6114038a07cd
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-07-14 16:05:15 -0400

    Almost forgot this one

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
