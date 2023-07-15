d47337e4efe311df52230049d0409ef3b607a710
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-07-15 05:08:02 +0300

    Advanced struggle:(

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int low;
+	int high;
+
+	for (high = 0; high < 9; high++)
+	{
+		for (low = 0; low <= 9; low++)
+		{
+			putchar(high + '0');
+			putchar(low + '0');
+		
+			if (low == 9 && high == 8)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
