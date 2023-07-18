b57fcf9854155dd34f0abf56136440c0bc9b4036
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-07-19 00:50:58 +0300

    finished of the mandatory;)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * add - Adds two values
+  * @x: The first number to be computed
+  * @y: The second number to be computed
+  *
+  * Return: Integer value
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
