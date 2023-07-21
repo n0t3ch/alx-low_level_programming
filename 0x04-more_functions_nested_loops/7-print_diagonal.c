43e44b51bd50479d74d40d76d5005a8c68acd3b5
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-07-22 00:04:01 +0300

    small changes

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	if (n < 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
@@ -20,7 +20,7 @@ void print_diagonal(int n)
 		{
 			for (b = 0; b < a; b++)
 				_putchar(' ');
-			_putchar('\\');
+			_putchar(92);
 			_putchar('\n');
 		}
 	}
