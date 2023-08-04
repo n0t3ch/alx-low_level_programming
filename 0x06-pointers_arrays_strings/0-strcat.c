86faf17dbb5df458f961ddf32c89b7560ad237ce
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-08-04 10:20:04 +0300

    concaternate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+	char *rdest;
+
+	rdest = dest;
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return rdest;
+}
