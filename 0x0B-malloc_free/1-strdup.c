b1c6fd9a8adaa4acee0fa92f31e9c90e88e5a339
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-10-19 22:30:03 +0300

    copy to memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+char *_strdup(char *str)
+{
+	unsigned int len, n;
+	char *cp;
+
+	len = 0;
+	while (*str)
+	{
+		str++;
+		len++;
+	}
+	cp = malloc(sizeof(char * (len + 1));
+	if (cp == NULL)
+		return (NULL);
+	for (n = 0; n < len; n++)
+	{
+		cp[n] = str[n];
+	}
+	return (cp);
