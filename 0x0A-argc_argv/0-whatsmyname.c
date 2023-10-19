3a14edd41d4714468dc61e4db3744791ff8bee91
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-10-19 08:36:36 +0300

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 on success
+ */
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
