5d14da17782d24929c200211cc10d74af0fb4c99
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-10-27 13:56:24 +0300

    join

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include "main.h"
 
+/**
+ * str_concat - concatenate strings
+ * @s1: first string
+ * @s2: second string
+ * Return: s1 + s2
+ */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
@@ -11,6 +18,7 @@ char *str_concat(char *s1, char *s2)
 	p =(char *)malloc(sizeof(char) * (len + 1));
 	if (p == NULL)
 		return (NULL);
-	p = strcat(s1, s2);
+	strcpy(p, s1);
+	strcat(p, s2);
 	return (p);
 }
