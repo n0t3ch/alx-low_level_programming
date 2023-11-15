264bee1359d4f7165b21a915698e99727a85e5a8
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-11-15 16:00:53 +0300

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reverse a string
+ * @s: pointer to string
+ * Return: null
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
