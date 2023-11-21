07b8d87c1f3e2cd6d614d85edc10df46794a6b07
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-11-21 23:18:42 +0300

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -25,17 +25,17 @@ char *str_concat(char *s1, char *s2)
 	if (new == NULL)
 		return (NULL);
 
-	while (*s1 != '\0')
+	len1 = len2 = 0;
+	while (s1[len1] != '\0')
 	{
-		*new = *s1;
-		new++;
-		s1++;
+		new[len1] = s1[len1];
+		len1++;
 	}
-	while (*s2 != '\0')
+	while (s2[len2] != '\0')
 	{
-		*new = *s2;
-		new++;
-		s2++;
+		new[len1] = s2[len2];
+		len1++;
+		len2++;
 	}
 	i = len1 + len2;
 	new[i] = '\0';
