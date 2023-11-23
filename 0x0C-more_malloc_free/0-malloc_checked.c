d6843620c2aa15adca3caf9ede1034744a37fc82
n0t3ch <n0t3ch.hunt3r@proton.me>
2023-11-23 22:02:11 +0300

    exit function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ *
+ * @b: char to be checked
+ * Return: ptr on success
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+
+	{
+		exit(98);
+	}
+	return (ptr);
+}
