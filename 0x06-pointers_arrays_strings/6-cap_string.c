29ff98c964a3bd7f9a498644cb22ff3aa31b9956
n0t3ch <n0t3ch.hunt3r@proton.me>
2024-03-28 14:18:59 +0300

    caPitalizE

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize every word
+ * @s: target string
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	char *up = s;
+	int cspc = 13;
+	int arri;
+	int stri = 0;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[stri])
+	{
+		arri = 0;
+		while (arri < cspc)
+		{
+			if ((stri == 0 || s[stri - 1] == spc[arri]) && (s[stri] > 96 && s[stri] < 123))
+				s[stri] -= 32;
+			arri++;
+		}
+		stri++;
+	}
+	return (up);
+}
