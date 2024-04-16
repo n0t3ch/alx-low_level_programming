e3679eb6ef9f9035ee698cdcb312a9890e7c5473
n0t3ch <n0t3ch.hunt3r@proton.me>
2024-04-16 11:07:45 +0300

    everything

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strchr - find character in string
+ * @s: pointer to string
+ * @c: characterto find
+ * Return: pointer to character
+ */
+char *_strchr(char *s, char c)
+{
+	int ind;
+	char *
